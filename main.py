d8a5e7cc1567ccfb5849f08bbfd401fba3b274e7
ekianjo <ekianjo@gmail.com>
2013-12-10 16:07:22 +0900

    Create main.py
    started

---
+++ main.py
@@ -0,0 +1,6 @@
+#text adventure game
+#gestion des personnages -> variables d'entree avec RPG-like parameters
+#actions a choix multiples -> qui donnent lieu a des variantes de situations -> differents enbranchements. 
+#episodique.
+#actions possibles a tout moment en plus des actions circonstantielles
+#script est separe du client
